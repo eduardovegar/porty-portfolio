0ea257d606db4c4b774ca250eab7552a6241293d
Eduardo Vega <eduardovegarct@gmail.com>
2018-11-22 19:55:21 -0800

    injected dependency dummy portfolio items

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using porty_portofolio.Services;
 
 namespace porty_portofolio
 {
@@ -33,6 +34,7 @@ namespace porty_portofolio
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IPortfolioItemService, DummyPortfolioItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
