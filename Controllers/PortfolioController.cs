8e4efe426a96a05179d4dd2369a1655204f74e0c
Eduardo Vega <eduardovegarct@gmail.com>
2018-11-22 19:57:27 -0800

    add return model from items

--- Controllers/PortfolioController.cs
+++ Controllers/PortfolioController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using porty_portofolio.Services;
+using porty_portofolio.Models;
 
 namespace porty_portofolio.Controllers
 {
@@ -18,8 +19,15 @@ namespace porty_portofolio.Controllers
 
     public async Task<IActionResult> Index()
     {
+      // get items assynchronally
       var items = await _portfolioItemService.GetIncompleteItemsAsync();
-      return View();
+
+      var model = new PortfolioViewModel()
+      {
+        Items = items
+      };
+
+      return View(model);
     }
 
   }
