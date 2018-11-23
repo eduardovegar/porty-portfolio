d149b16509683b91ad961e74541117420da2fd71
Eduardo Vega <eduardovegarct@gmail.com>
2018-11-22 19:51:25 -0800

    add portfolio service injection

--- Controllers/PortfolioController.cs
+++ Controllers/PortfolioController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using porty_portofolio.Services;
 
 namespace porty_portofolio.Controllers
 {
   public class PortfolioController : Controller
   {
-    public IActionResult Portfolio()
+    private readonly IPortfolioItemService _portfolioItemService;
+
+    public PortfolioController(IPortfolioItemService portfolioItemService)
     {
+      _portfolioItemService = portfolioItemService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      var items = await _portfolioItemService.GetIncompleteItemsAsync();
       return View();
     }
 
