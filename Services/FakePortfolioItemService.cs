a7017764c3144d3cf3474fefa4fff2c4842ab8c0
Eduardo Vega <eduardovegarct@gmail.com>
2018-11-26 19:06:52 -0800

    added new mockup items from actual projects

--- Services/FakePortfolioItemService.cs
+++ Services/FakePortfolioItemService.cs
@@ -24,7 +24,29 @@ namespace porty.Services
         ImageUrl = "http://54.71.87.202/wp-content/uploads/2018/10/image.png"
       };
 
-      return Task.FromResult(new[] {item1, item2}); // returns task array from created items
+      var item3 = new PortfolioItem
+      {
+        Title = "Model United Nations Club Logo",
+        Description = "Designed the FDU MUN Club Logo from scratch, using key elements such as the UN globe to represent union, the olive branches that represent peace, and the use of Playfair Display as a font for a touch of modernism and seriousness",
+        PublishedAt = DateTimeOffset.Now,
+        ImageUrl = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2e42f872054695.5bda1cbcc1881.png"
+      };
+      var item4 = new PortfolioItem
+      {
+        Title = "Finch's Tea and Coffee House",
+        Description = "Designed and deployed the website for Finch's, a local tea shop in Vancouver",
+        PublishedAt = DateTimeOffset.Now,
+        ImageUrl = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b695db52171277.5907b2df44b9b.png"
+      };
+      var item5 = new PortfolioItem
+      {
+        Title = "ROBUSTA - A Minimal Coffee Magazine",
+        Description = "As part of a class project, designed, constructed and published (printed, as well) a concept magazine called ROBUSTA.",
+        PublishedAt = DateTimeOffset.Now,
+        ImageUrl = "https://mir-s3-cdn-cf.behance.net/project_modules/disp/4f853652171137.5907b16867dda.png"
+      };
+
+      return Task.FromResult(new[] {item1, item2, item3, item4, item5}); // returns task array from created items
     }
   }
 }
