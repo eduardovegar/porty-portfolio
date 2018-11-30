5fd6e6cf534c5c3f6c8dd49648c7a6828c9ff15d
Ana C <anairiscabrera17@gmail.com>
2018-11-29 18:33:46 -0800

    added new controller for public portfolios

---
+++ Controllers/PublicPortfoliosController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace porty.Controllers
+{
+    public class PublicPortfoliosController : Controller
+    {
+  
+    public IActionResult Index()
+    {
+
+    }
+  }
+
+}
